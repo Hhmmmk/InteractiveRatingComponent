aaec0e43e1ebd2cf506049d9798f9905b960b803
Hhmmmk <lesly.celzo@gmail.com>
2022-06-13 17:54:18 +0800

    Cleaned up js

--- main.js
+++ main.js
@@ -4,15 +4,11 @@ const rating = document.querySelector('.rating');
 const submitRate = document.querySelector('.submit');
 
 function removeSelected() {
-  for (j = 0; j < buttons.length; j++) {
+  for (let j = 0; j < buttons.length; j++) {
     buttons[j].classList.remove('selected');
   }
 }
 
-
-
-
-// Attempting to make this work instead of the one below but i don't know what i am doing wrong
 
 for (let j = 0; j < buttons.length; j++) {
   buttons[j].addEventListener('click', () => {
@@ -24,55 +20,7 @@ for (let j = 0; j < buttons.length; j++) {
 }
 
 
-
-// buttons[0].addEventListener('click', () => {
-//   removeSelected();
-//   buttons[0].classList.add('selected');
-//   rate = buttons[0].innerHTML;
-//   rating.innerHTML = rate;
-// })
-
-// buttons[1].addEventListener('click', () => {
-//     removeSelected();
-//     buttons[1].classList.add('selected');
-//     rate = buttons[1].innerHTML;
-//     rating.innerHTML = rate;
-// })
-
-// buttons[2].addEventListener('click', () => {
-//   removeSelected();
-//   buttons[2].classList.add('selected');
-//   rate = buttons[2].innerHTML;
-//   rating.innerHTML = rate;
-// })
-
-// buttons[3].addEventListener('click', () => {
-//   removeSelected();
-//   buttons[3].classList.add('selected');
-//   rate = buttons[3].innerHTML;
-//   rating.innerHTML = rate;
-// })
-
-// buttons[4].addEventListener('click', () => {
-//   removeSelected();
-//   buttons[4].classList.add('selected');
-//   rate = buttons[4].innerHTML;
-//   rating.innerHTML = rate;
-// })
-
 submitRate.addEventListener('click', () => {
   const card2Hidden = document.querySelector('.hide');
   card2Hidden.classList.remove('hide');
 })
-
-
-
-
-// for (h = 0; h < buttons.length; h++) {
-//   buttons[h].addEventListener('click', () => {
-//     removeSelected();
-//   })
-//   buttons[h].classList.add('selected');
-// }
-
-
